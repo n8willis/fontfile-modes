1820b3293b2a8942ebc4ef852477cce7564b13ec
Nathan Willis <nwillis@glyphography.com>
2015-04-30 12:25:54 -0400

    Turn on simple XML element folding.

--- fontfile-ttx-mode.el
+++ fontfile-ttx-mode.el
@@ -37,6 +37,29 @@
 
 
 ;;
+;; Folding elements
+;;
+(require 'hideshow)
+(require 'sgml-mode)
+(require 'nxml-mode)
+
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+
+
+
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+
+;; optional key bindings, easier than hs defaults
+(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
+
+;;
 ;; Eldoc documentation hints
 ;;
 
--- fontfile-ufo-mode.el
+++ fontfile-ufo-mode.el
@@ -23,6 +23,29 @@
 (push (cons (concat "\\." (regexp-opt '("glif") t)
                       "\\'") 'nxml-mode) auto-mode-alist)
 
+;;
+;; Folding elements
+;;
+(require 'hideshow)
+(require 'sgml-mode)
+(require 'nxml-mode)
+
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+
+
+
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+
+;; optional key bindings, easier than hs defaults
+(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
+
 
 ;;
 ;; Entry point
